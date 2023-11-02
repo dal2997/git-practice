59fd8be34d16510f7c94cabc59909f272f0f32bc
dal2997 <ys2997@naver.com>
2023-11-02 18:39:38 +0900

    html과 js 연결

---
+++ hinamizawa/script.js
@@ -0,0 +1,47 @@
+
+// console.clear();
+// console.log('hi');
+/*
+let a;
+a=10;
+a=20;
+let b;
+*/
+
+/*
+let age = 20;
+if(){
+}
+else if(){
+}
+else{
+}
+*/
+
+/*
+let i = 1;
+while(i<=10){
+  console.log(i);
+  i++; or i+=10; 보폭을 의미함. 보폭이 작으면 반복이 많아지겠지.
+}
+*/
+
+/*
+console.clear();
+for(let i = 1; i <= 10; i++){
+  console.log(i);
+}
+*/
+
+console.clear();
+console.log("==dan 시작==");
+for(let dan = 2; dan <=8; dan++){
+  console.log("dan : "+dan);
+  
+  console.log("==i 시작==");
+  for(let i = 1; i<=2; i++){
+    console.log(i);
+  }
+  console.log("==i 끝==");
+}
+console.log("==dan 끝==")
