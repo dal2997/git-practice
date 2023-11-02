0bbe1f9d744297b38f39030fed78c0bf50ebd26e
dal2997 <ys2997@naver.com>
2023-11-02 20:36:49 +0900

    js 구구단부터 3의배수까지

--- hinamizawa/script.js
+++ hinamizawa/script.js
@@ -1,14 +1,15 @@
 
 // console.clear();
 // console.log('hi');
-/*
+
+/*let 선언?
 let a;
 a=10;
 a=20;
 let b;
 */
 
-/*
+/*if else
 let age = 20;
 if(){
 }
@@ -18,7 +19,7 @@ else{
 }
 */
 
-/*
+/*while
 let i = 1;
 while(i<=10){
   console.log(i);
@@ -26,13 +27,14 @@ while(i<=10){
 }
 */
 
-/*
+/*for
 console.clear();
 for(let i = 1; i <= 10; i++){
   console.log(i);
 }
 */
 
+/*for 이중반복문
 console.clear();
 console.log("==dan 시작==");
 for(let dan = 2; dan <=8; dan++){
@@ -45,3 +47,62 @@ for(let dan = 2; dan <=8; dan++){
   console.log("==i 끝==");
 }
 console.log("==dan 끝==")
+*/
+
+/* 19단
+console.clear();
+for(let dan=2; dan<=19; dan++){
+  console.log("== 구구단 "+dan+"단 ==");
+
+  for(let i =1; i<=19; i++){
+    console.log(dan+"*"+ i +"="+dan*i);
+  }
+}
+*/
+
+/*1부터 10사이의 짝수만 출력
+console.log("==1부터 10사이의 짝수만 출력==");
+for(let i =1; i<=10; i++){
+  if(i%2==0){
+    console.log(i);
+  }
+}
+*/
+
+/*1부터 10사이의 3의 배수만 출력
+console.log("==1부터 10사이의 짝수만 출력==");
+for(let i =1; i<=10; i++){
+  if(i%3==0){
+    console.log(i);
+  }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
